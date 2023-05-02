81b86c0f94b9f8fb045f8e2ef18f7dccfe9840ed
websolucionOrg <131605739+websolucionOrg@users.noreply.github.com>
2023-05-02 07:41:58 +0000

    assasa

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import { Store } from '@/utils/Store';
 
 const Layout = ({ title ,children}) => {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <>
      <Head>
@@ -16,7 +19,13 @@ const Layout = ({ title ,children}) => {
              <nav className='flex h-12 items-center justify-between p-4 shadow-md'>
                    <Link href="/" className='text-lg font-bold'>Amazonas </Link>
                    <div>
-                   <Link href="/" className=""> Cart </Link>
+                   <Link href="/" className="">  
+                   {cart.cartItems.length > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </span>
+                  )}
+                   </Link>
                    <Link href="/" className="">Login</Link>
                    </div>
              </nav>
--- pages/proct/[slug].js
+++ pages/proct/[slug].js
@@ -3,19 +3,72 @@ import Layout from '@/components/Layout'
 import { useRouter } from 'next/router'
 import data from '@/utils/data'
 import Image from 'next/image'
-export default function ProductScreen(){
+import Link from 'next/link'
+import { useContext } from 'react'
+import { Store } from '@/utils/Store'
 
+export default function ProductScreen(){
+  const { state, dispatch } = useContext(Store);
+  //Esquerar uma querystring e compara-la mcom a os dados existentes
     const {query} = useRouter();
     const {slug} = query;
     const product =  data.products.find((X) => X.slug === slug );
     if (!product) {
         return ( <div>Não Existe o producto</div>)
     }
+
+//fim de 
+
+
+//add produto no carinho
+    const addToCartHandler = () => {
+      const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+      const quantity = existItem ? existItem.quantity + 1 : 1;
+  
+      if (product.countInStock < quantity) {
+        alert('Sorry. Product is out of stock');
+        return;
+      }
+  
+      dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    };
+  
   return (
     <Layout title={product.name}>
-     <h1> {product.name} </h1>
-     <Image src={product.image} width={400} height={400} alt={product.name} ></Image>
-     
+      <diV classNanme="py-2">
+     <Link href="/">Voltar aos produtos</Link>
+      </diV>
+      <div className='grid md:grid-cols-4 md:gap-3'>
+        <div className='md:col-span-2'>
+    
+     <Image src={product.image} width={400} height={400} alt={product.name}  layout='responsive'></Image>
+        </div>
+        <div>
+          <ul>
+            <li><h1> {product.name} </h1></li>
+            <li> {product.brand} </li>
+            <li> {product.rating} de {product.numReviews} visualizações </li>
+            <li> Descrição: {product.description} </li>
+          </ul>
+           
+        </div>
+          <div>
+            <div className='card p-5' >
+              <div className='mb-2 p-2 shadow-md flex justify-between' >
+                 <div> Preço </div>
+                <div> {product.price} Kz </div>   
+              </div>
+
+              <div className='mb-2 p-2 shadow-md flex justify-between' >
+                 <div> Estatuto </div>
+                <div> {product.countInStock >  0 ? "Em stoke": "Não Avaliado" }  </div>   
+              </div>
+              <button className='primary-button w-full'  onClick={addToCartHandler} > Adicionar no </button>
+            </div>
+          </div>
+      </div>
+
+
     </Layout>
   )
 }
